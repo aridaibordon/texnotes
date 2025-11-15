a245a238437904be33af1bc26a7ac7aba164d756
Aridai Bordón <aridaybordon@gmail.com>
2025-11-15 21:36:36 +0000

    add local .templates folder

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
 
-NOTE_FOLDER = Path.home() / "Documents" / "notes"
-PAPER_FOLDER = Path.home() / "Documents" / "papers"
-TEMPLATES_FOLDER = Path.home() / "Documents" / ".templates"
+NOTE_PATH = Path.home() / "Documents" / "notes"
+PAPER_PATH = Path.home() / "Documents" / "papers"
+TEMPLATES_PATH = Path(__file__).parent / ".templates"
 
-NOTE_TEMPLATE = TEMPLATES_FOLDER / "note.tex"
-PAPER_TEMPLATE = TEMPLATES_FOLDER / "paper.tex"
+NOTE_TEMPLATE = TEMPLATES_PATH / "note.tex"
+PAPER_TEMPLATE = TEMPLATES_PATH / "paper.tex"
